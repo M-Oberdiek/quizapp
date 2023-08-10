de98e5e536938575a9324800c4831e65b9431b82
Marcus Oberdiek <moberdiek@gmx.de>
2023-08-10 17:48:12 +0200

    add code structure for ne cards

--- js/index.js
+++ js/index.js
@@ -8,4 +8,22 @@ const answer = document.querySelector('[data-js="answer"]');
 const showAnswer = document.querySelector('[data-js="show-button"]');
 showAnswer.addEventListener("click", () => {
   answer.classList.toggle("answer--show");
+
+  if (answer.classList.contains("answer--show")) {
+    showAnswer.innerText = "Hide answer";
+  } else if (answer.classList.contains("answer")) {
+    showAnswer.innerText = "Show Answer";
+  }
 });
+
+const cardContainer = document.querySelector('[data-js="card-container"]');
+const addButton = document.querySelector('[data-js="add-button"]');
+
+function addCard(question, answer, categorie) {
+  const newCard = document.createElement("li");
+  newText.innerText = text;
+  newText.classList.add("card-container__newCard");
+  cardContainer.appendChild(newText);
+  console.log(newText);
+}
+addCard(A, Q, C);
