54976c756c30b85764b7fa6e1785b8db42821d87
Marcus Oberdiek <moberdiek@gmx.de>
2023-08-10 14:46:47 +0200

    add mark bookmark and show answer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-const buttonToggle = document.querySelector('[data-js="bookmark"]');
-buttonToggle.addEventListener("click", () => {
-  bookmarkElement.classList.toggle("activ");
+const bookmark = document.querySelector('[data-js="bookmark"]');
+console.log(bookmark);
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("bookmark--activ");
+});
+
+const answer = document.querySelector('[data-js="answer"]');
+const showAnswer = document.querySelector('[data-js="show-button"]');
+showAnswer.addEventListener("click", () => {
+  answer.classList.toggle("answer--show");
 });
